21d545849ef55f4bb2cee7f1f1ac91af2bb36b37
GermanScillato <germanscillato3@gmail.com>
2022-06-17 19:20:49 -0300

    Def func grafico Manos , ordenar config

--- data/config.py
+++
@@ -1,6 +0,0 @@
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-
-class config_graficos():
-    def __init__(self):
-        myColors = ((0.90, 0.96, 1, 1), (0.70, 0.87, 1, 1), (0, 0.40, 0.75, 0.88))
-        cmap = LinearSegmentedColormap.from_list('Custom', myColors,10)
