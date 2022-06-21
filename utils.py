65f22558d1e7e55853d74f89116f67f5f1cafcac
GermanScillato <germanscillato3@gmail.com>
2022-06-20 22:16:08 -0300

    Ultima revision

---
+++ utils.py
@@ -0,0 +1,89 @@
+
+import os
+from os import path
+import time
+import re
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import seaborn as sns
+
+import requests
+import warnings
+
+from nltk.tokenize import word_tokenize
+from nltk import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+from nltk.corpus import stopwords
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import roc_curve
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.decomposition import TruncatedSVD
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.ensemble import AdaBoostClassifier, ExtraTreesClassifier , RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+from PIL import Image
+
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+from tqdm import tqdm
+
+from lime.lime_tabular import LimeTabularExplainer
+
+from xgboost import XGBClassifier
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+
+import pickle
+
+warnings.filterwarnings('ignore')
+
+stopwords_en = stopwords.words('english')
+porter = PorterStemmer()
+stopwords_en_porter = [porter.stem(x) for x in stopwords_en]
+
+# Agrego Stopwords específicas del dataset.
+def append_stopword(listado_stopwords, stopwords_agregar):
+    for stopwords in stopwords_agregar:
+        listado_stopwords.append(stopwords)
+    return listado_stopwords
+
+stopwords_agregar = ['reuter','said','Reuters','via','imag','https','com','one',
+'u','also','would','featur','pic','us','wednesday','friday','monday','tuesday',
+'saturday','sunday','thursday','getti','read','gop','watch','donald','trump',
+'hillari','mr','accord','america','seem','youtub','21st',
+'video' , 'http' , 'like' , 'obama' , 'minist' , 'washington' , 'know' , ]
+
+stopwords_en_porter = append_stopword(stopwords_en_porter ,stopwords_agregar )
+
+# Definición de función para el tokenizer
+def porter_tokenizer(text):
+    ''' Esta función se utiliza cuando se sobre escribe el proceso
+    de tokenización en la clase CountVectorizer para que se pueda
+     obtener también la raíz de la palabra.'''
+    words = word_tokenize(text)
+    words = [re.sub('[^A-Za-z0-9]+', '', word) for word in words]  # remover los caracteres especiales
+    words = [word.replace(' ', '') for word in words] #remover los espacios en blanco
+    words = [word for word in words if word != '']
+    words = [PorterStemmer().stem(word) for word in words] # obtener la raiz de las palabras
+    words = [word for word in words if word not in stopwords_en_porter] # remover las stopwords
+    return words
